1dc22e29060bb8bffe8299a2ca1550760615c8af
ssarma13 <sharma.joey17@gmail.com>
2021-12-07 21:24:13 -0500

    ReviewItem  html

--- front-end/work-for-u/components/common/rating/reviewItem.js
+++ front-end/work-for-u/components/common/rating/reviewItem.js
@@ -3,7 +3,16 @@ import { Rating } from "./rateModal"
 
 export const ReviewItem = ({reviewer, date, title, review, rating}) => {
     return(
-        <>
-        </>
+        <Box boxShadow={'md'} p="10px" w={'100%'} borderRadius={'10px'}>
+            <HStack spacing={'20px'}>
+                <Avatar size={'md'} name={reviewer.fullName}/>
+                <Box>
+                    <Heading fontSize={'xl'}>{reviewer.fullName}</Heading>
+                    <Rating disabled={true} rating={rating}/>
+                </Box>
+            </HStack>
+            <Text m="10px 0" fontSize='lg' fontWeight={'bold'}>{title}</Text>
+            <Text m="10px 0" fontSize='lg' fontStyle={'italic'}>"{review}"</Text>
+        </Box>
     )
 }
