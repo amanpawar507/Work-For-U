45902699fd8509a80e55fe0f57f59a18d2bca705
amanpawar507 <amanpawar507@gmail.com>
2021-12-10 12:40:20 -0500

    Implemented filters in search

--- front-end/work-for-u/components/common/freelancerList.js
+++ front-end/work-for-u/components/common/freelancerList.js
@@ -1,55 +1,95 @@
-import { HStack, SimpleGrid, useDisclosure,
-    Menu,
-    MenuButton,
-    MenuList,
-    MenuItem,
-    MenuItemOption,
-    MenuGroup,
-    MenuOptionGroup,
-    MenuIcon,
-    MenuCommand,
-    MenuDivider,
-    Button,
-    Select,
-} from "@chakra-ui/react"
+import {
+  HStack,
+  SimpleGrid,
+  useDisclosure,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuItemOption,
+  MenuGroup,
+  MenuOptionGroup,
+  MenuIcon,
+  MenuCommand,
+  MenuDivider,
+  Button,
+  Select,
+} from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { MdKeyboardArrowDown } from "react-icons/md";
-import { FreelancerCard } from "../dashboard/freelancerCard"
-import { RequestModal } from "../dashboard/requestModal"
+import { FreelancerCard } from "../dashboard/freelancerCard";
+import { RequestModal } from "../dashboard/requestModal";
 
-export const FreelancerList = ({list}) => {
-    const [selectedFreelancer, setSelectedFreelancer] = useState(null);
-    const [resultList, setResultList] = useState([]);
-    const {isOpen, onOpen, onClose} = useDisclosure();
+export const FreelancerList = ({ list }) => {
+  const [selectedFreelancer, setSelectedFreelancer] = useState(null);
+  const [resultList, setResultList] = useState([]);
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-    useEffect(()=> {
-        if(!list) return;
-        setResultList(list);
-    },[list])
+  useEffect(() => {
+    if (!list) return;
+    setResultList(list);
+  }, [list]);
 
+  const handleRequestOpen = (freelancer) => {
+    setSelectedFreelancer(freelancer);
+    onOpen();
+  };
 
-    const handleRequestOpen = freelancer => {
-        setSelectedFreelancer(freelancer);
-        onOpen();
+  const handleRatingSort = (e) => {
+    const { value } = e.target;
+    let current = [];
+    switch (value) {
+      case "ascRating":
+        current = list.sort((a, b) => {
+          return a.overallRating - b.overallRating;
+        });
+        break;
+      case "descRating":
+        current = list.sort((a, b) => {
+          return b.overallRating - a.overallRating;
+        });
+        break;
+      case "ascSuccess":
+        current = list.sort((a, b) => {
+          return a.successRate - b.successRate;
+        });
+        break;
+      case "descSuccess":
+        current = list.sort((a, b) => {
+          return b.successRate - a.successRate;
+        });
+        break;
+      default:
+        break;
     }
+    setResultList([...current]);
+  };
 
-  
+  return (
+    <>
+      <Select placeholder="Sort By" w={"200px"} onChange={handleRatingSort}>
+        <option value="ascRating">Low to High Rating</option>
+        <option value="descRating">High to Low Rating</option>
+        <option value="ascSuccess">Low to High Success Rate</option>
+        <option value="descSuccess">High to Low Success Rate</option>
+      </Select>
 
-     return(
-        <>
-            <SimpleGrid mt='10px' columns={[2, null, 3]} spacing='40px'>
-                {resultList.map(i => 
-                    <FreelancerCard 
-                        id={i._id}
-                        name={i.fullName} 
-                        rating={i.overallRating} 
-                        location={i.location}
-                        onRequest={() => handleRequestOpen(i)}    
-                    />    
-                )}
-            </SimpleGrid>
-            <RequestModal isOpen={isOpen} onClose={() => onClose()} selectedFreelancer={selectedFreelancer}/>
-        </>
-
-    )
-}
+      <SimpleGrid mt="10px" columns={[2, null, 3]} spacing="40px">
+        {resultList.map((i) => (
+          <FreelancerCard
+            id={i._id}
+            name={i.fullName}
+            rating={i.overallRating}
+            location={i.location}
+            onRequest={() => handleRequestOpen(i)}
+          />
+        ))}
+      </SimpleGrid>
+      <RequestModal
+        isOpen={isOpen}
+        onClose={() => onClose()}
+        selectedFreelancer={selectedFreelancer}
+      />
+    </>
+  );
+};
