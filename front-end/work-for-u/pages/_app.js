2aa3ae7332e9a550f328401a36d1a785c1f6383f
Saurabh Mane <saurabhmane1997@gmail.com>
2021-12-12 22:39:59 -0500

    introduction problem

--- front-end/work-for-u/pages/_app.js
+++ front-end/work-for-u/pages/_app.js
@@ -1,17 +1,46 @@
 import '../styles/globals.css'
-import { ChakraProvider } from "@chakra-ui/react"
+import { Button, ChakraProvider, Container, HStack, Text } from "@chakra-ui/react"
 import theme from '../theme'
 import "@fontsource/roboto"
+import {ErrorBoundary} from "react-error-boundary";
 import UserContextProvider from '../components/contexts/userContext'
+import { useRouter } from 'next/dist/client/router';
 
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
   return <ChakraProvider theme={theme}>
-    <UserContextProvider>
+    <ErrorBoundary
+      FallbackComponent={Error}
+      onReset={() => {
+      router.push("/logout");
+      }}
+
+        
+    >
+      <UserContextProvider>
       <Component {...pageProps} />
     </UserContextProvider>
+    </ErrorBoundary>
   </ChakraProvider>
 
+}
+
+const Error = ({ error, componentStack, resetErrorBoundary }) => {
+
+
+
+
+
+  return(
+      <Container display={'flex'} justifyContent={'center'} flexDirection={'column'} h={'100vh'}>
+          <Text w={'100%'} textAlign={'center'} fontSize={'xl'}>Oops! Something went wrong !</Text>
+          <HStack w={'100%'} justifyContent={'center'} mt='20px'>
+              <Button variant={'outline'} onClick={resetErrorBoundary} >Go to login</Button>
+          </HStack>
+      </Container>
+  )
 }
 
 export default MyApp
